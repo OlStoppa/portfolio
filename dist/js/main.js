ec06c97304ef407f4d01d9905c3bc89e2dfacbd5
OlStoppa <olthespud@hotmail.com>
2019-03-26 11:36:21 +0800

    added animations

---
+++ dist/js/main.js
@@ -0,0 +1,31 @@
+
+    window.sr = ScrollReveal({reset: true});
+		sr.reveal( ".icons", {
+			duration: 3000,
+			origin: "left",
+			distance: '200px'
+        });
+       
+        sr.reveal( ".button__portfolio--wrapper", {
+			duration: 4000,
+            origin: "bottom",
+            distance: '200px'
+		
+        });
+        sr.reveal( ".sm-heading", {
+			duration: 2000,
+            origin: "right",
+            distance: '500px'
+		
+        });
+        sr.reveal( ".project-img", {
+			duration: 1000,
+            origin: "bottom",
+            distance: '500px',
+            viewFactor: 0.2
+		
+        });
+       
+
+
+        
